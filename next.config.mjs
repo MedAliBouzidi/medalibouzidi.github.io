741c3e3715c7ed171ab60a4fef6f8bb223559d87
BOUZIDI Med Ali <bouzidi.medali20@gmail.com>
2024-10-07 17:54:19 +0100

    publish testing

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-/**
- * Error: Invalid src prop (https://images.pexels.com/photos/18150916/pexels-photo-18150916/free-photo-of-woman-hand-pouring-water-and-brewing-coffee.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2) on `next/image`, hostname "images.pexels.com" is not configured under images in your `next.config.js`
- */
-nextConfig.images = {
-    domains: ['images.pexels.com'],
+const nextConfig = {
+    // output: "export",
+    basePath:  '/portfolio',
+    reactStrictMode: true,
+    images: {
+        domains: ['images.pexels.com'],
+    }
 };
 
 export default nextConfig;
